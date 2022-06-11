9e703bdaf2f11da769705ab00c8e43f6d9954186
alixb <null@none.com>
2022-06-11 15:23:40 +0200

    Animate InteractRaycast2D in the Hero scene with an AnimationTree (State Machine)

---
+++ note_block_audio_stream_player_2d.gd
@@ -0,0 +1,17 @@
+class_name NoteBlockAudioStreamPlayer2D
+extends AudioStreamPlayer2D
+
+
+# ----------------- DECLARE VARIABLES -----------------
+
+
+
+# ----------------- RUN CODE -----------------
+
+
+
+# ----------------- DECLARE FUNCTIONS -----------------
+
+
+func _on_NoteBlock_interactable_enabled() -> void:
+	self.play()
