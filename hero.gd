7a81099eef4660ad1e3b9b5f148e90a51e11b8e4
alixb <null@none.com>
2022-06-11 15:28:49 +0200

    Write some code to link the AnimationTree and the script together

--- hero.gd
+++ hero.gd
@@ -10,12 +10,19 @@ export var speed: float = 250.0
 var direction: Vector2 = Vector2(0.0, 0.0)
 var velocity: Vector2 = Vector2(0.0, 0.0)
 
+onready var animation_tree: AnimationTree = $AnimationTree
+onready var animation_tree_node_sm_playback = animation_tree.get("parameters/playback")
+
+
 # ----------------- RUN CODE -----------------
 
 func _physics_process(delta: float) -> void:
-#	get_all_inputs()
-	
-#	velocity = move_and_slide(velocity)
+	if not self.velocity == Vector2(0.0, 0.0):
+		# Set the animation Tree to play Animation Move with the velocity
+		self.animation_tree.set("parameters/Move/blend_position", self.velocity)
+		self.animation_tree_node_sm_playback.travel("Move")
+	else:
+		self.animation_tree_node_sm_playback.travel("Idle")
 	return
 
 # ----------------- DECLARE FUNCTIONS -----------------
