5120683d14101d8a944a1983963c98f5d189622a
alixb <null@none.com>
2022-06-17 15:55:43 +0200

    Make the PauseMenu actually pause the game via a key input and make its buttons return to MainMenu or resume game

--- pause_menu.gd
+++ pause_menu.gd
@@ -18,17 +18,52 @@ func _ready() -> void:
 	return
 
 
+func _unhandled_key_input(_event: InputEventKey) -> void:
+	if Input.is_action_just_pressed("pause_menu"):
+#		toggle_paused()
+		self.visible = !visible
+	return
+
+
 # ----------------- DECLARE FUNCTIONS -----------------
 
 
+func set_paused(paused: bool) -> void:
+	get_tree().paused = paused
+	self.visible = paused
+	
+	if paused:
+		self.first_button_to_focus.grab_focus()
+	else:
+		self.first_button_to_focus.release_focus()
+	return
+
+
+func toggle_paused() -> void:
+	if not get_tree().paused:
+		set_paused(true)
+	else:
+		set_paused(false)
+	return
+
 func _initialize_asserts() -> void:
 	assert(self.first_button_to_focus_node_path != null)
-	
+	return
+
 
 func _initialize() -> void:
+	self.hide()
 	self.first_button_to_focus.grab_focus()
+	return
 
 
 func _on_ResumeTextureButton_pressed() -> void:
-	pass
+	self.hide()
 	return
+
+
+func _on_PauseMenu_visibility_changed() -> void:
+	if visible:
+		set_paused(true)
+	else:
+		set_paused(false)
