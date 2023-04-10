1763f54f41845f700f94d61f6accfbab86bf5722
alixbarreaux <alixbarreaux@github.com>
2023-04-10 18:11:17 +0200

    Feature: Make DialogueGUI able to process received dialogue data

--- test/dialogue_gui.gd
+++ test/dialogue_gui.gd
@@ -41,30 +41,30 @@ func _initialize() -> void:
 
 var first_input_triggered_once: bool = false
 
-func _gui_input(event: InputEvent) -> void:
-	if event is InputEventMouseButton:
-		if (event.get_button_index() == 1) or (event.get_button_index() == 2):
-			if not event.pressed:
-				if not first_input_triggered_once:
-					first_input_triggered_once = true
-					return
-				
-				if is_last_speech:
-					animation_tree_anim_node_state_machine_playback.travel("End")
-					return
-				
-				play_next_speech()
-				self.accept_event()
-	return
+#func _gui_input(event: InputEvent) -> void:
+#	if event is InputEventMouseButton:
+#		if (event.get_button_index() == 1) or (event.get_button_index() == 2):
+#			if not event.pressed:
+#				if not first_input_triggered_once:
+#					first_input_triggered_once = true
+#					return
+#
+#				if is_last_speech:
+#					animation_tree_anim_node_state_machine_playback.travel("End")
+#					return
+#
+#				play_next_speech()
+#				self.accept_event()
+#	return
 
 
 func _unhandled_input(event: InputEvent) -> void:
-	if (event is InputEventKey) or (event is InputEventJoypadButton):
+	if Input.is_action_just_released("ui_confirm_dialogue"): 
 		if not event.pressed:
 			if not first_input_triggered_once:
 				first_input_triggered_once = true
 				return
-				
+			
 			if is_last_speech:
 				animation_tree_anim_node_state_machine_playback.travel("End")
 				return
@@ -76,41 +76,50 @@ func _unhandled_input(event: InputEvent) -> void:
 
 # ----------------- DECLARE FUNCTIONS -----------------
 
+
 # This helps knowing which animation to play when ending the dialogue
 var is_last_speech: bool = false
 
 func play_next_speech() -> void:
-	if dialogue.data[speech_index_increment].message != "":
-		message_rich_text_label.bbcode_text = dialogue.data[speech_index_increment].message
-
-
-	if dialogue.data[speech_index_increment].texture_file_path != "":
-		speaker_texture.texture = load(dialogue.data[speech_index_increment].texture_file_path)
+#	if dialogue.data[speech_index_increment].message != "":
+#		message_rich_text_label.bbcode_text = dialogue.data[speech_index_increment].message
+#
+#
+#	if dialogue.data[speech_index_increment].texture_file_path != "":
+#		speaker_texture.texture = load(dialogue.data[speech_index_increment].texture_file_path)
 	
 #	if dialogue.data[speech_index_increment].sound_file_path != "":
-#
 
 
-	if is_last_speech:
-		self.play_speech_at_index( dialogue.data.size() -1 )
-		speech_index_increment = 0
-		pass
 	
+#
 	if speech_index_increment == ( dialogue.data.size() -1 ):
 		is_last_speech = true
-		self.play_next_speech()
-		pass
+#		self.play_next_speech()
+		print("Last speech detected, index: ", speech_index_increment)
+		play_speech_at_index(speech_index_increment)
+		speech_index_increment = 0
+		return
 	
+#	if is_last_speech:
+#		speech_index_increment = 0
+#		self.play_speech_at_index( dialogue.data.size() -1 )
+#		return
+
+	print("NOT last speech, playing at index: ", speech_index_increment)
+	play_speech_at_index(speech_index_increment)
 	speech_index_increment += 1
 
 
 func play_speech_at_index(speech_to_play_index: int) -> void:
+	print(self.name, ": Playing speech at index: ", speech_to_play_index)
 	message_rich_text_label.bbcode_text = dialogue.data[speech_to_play_index].message
 #	$Panel/SpeakerTexture.texture = dialogue.data[speech_to_play_index].texture_file_path
 
 
 func stop() -> void:
 	print(self.name, ": The dialog has stopped, all speech lines have been played!")
+	is_last_speech = false
 	toggle_enabled(false)
 
 
