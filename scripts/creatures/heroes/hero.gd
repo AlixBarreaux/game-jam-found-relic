826efd9574059a1956a2cac35a2c877d3595d57e
alixbarreaux <alixbarreaux@github.com>
2023-04-20 22:37:26 +0200

    Feature: Adjust heroes speed

--- scripts/creatures/heroes/hero.gd
+++ scripts/creatures/heroes/hero.gd
@@ -6,7 +6,7 @@ extends KinematicBody2D
 
 export var is_controlled: bool = true
 
-export var speed: float = 400.0
+export var speed: float = 600.0
 
 var direction: Vector2 = Vector2(0.0, 0.0)
 var velocity: Vector2 = Vector2(0.0, 0.0)
